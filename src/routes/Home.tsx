b8f678f8afc758b9b7378941c2d613f0ecc78206
zlenner <119705166+zlenner@users.noreply.github.com>
2024-01-19 00:45:37 +0200

    fix

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,11 +1,8 @@
-import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet'
 import provincialSeatsGeoJson from '../geojson/provincial.json'
 import candidatesJson from '../geojson/candidates.json'
 import { Candidate, ProvincialGeoJson } from '../models'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { polygonStyle } from '../mapping/styles'
-import PTIElectionSymbol from '../assets/nobg.png'
-import { FaLocationCrosshairs } from 'react-icons/fa6'
 import Header from './components/Header'
 import Map from './Map'
 import BackgroundImage from './components/BackgroundImage'
@@ -15,7 +12,6 @@ const provincialSeats = provincialSeatsGeoJson as ProvincialGeoJson
 
 function App() {
   const { seat = null } = useParams()
-  const navigate = useNavigate()
 
   const currentFeature = provincialSeats.features.find(
     (f) => f.properties.PA === seat
@@ -29,28 +25,6 @@ function App() {
       ((candidatesJson as any)[currentFeature.properties.PA] as Candidate)
   }
 
-  // Attach event handlers to each feature
-  const onEachFeature = (feature: any, layer: any) => {
-    layer.on({
-      mouseover: (event: any) => {
-        const layer = event.target
-        layer.setStyle({
-          fillOpacity: 0.9
-        })
-      },
-      mouseout: (event: any) => {
-        const layer = event.target
-        layer.setStyle({
-          fillOpacity: 0.6
-        })
-      },
-      click: (event: any) => {
-        const layer = event.target
-        navigate('/' + layer.feature.properties.PA)
-      }
-    })
-  }
-
   return (
     <div className="flex flex w-full h-full">
       <div
@@ -59,12 +33,12 @@ function App() {
       >
         <Header goToMyConstituency={() => {}} />
         <div className="flex flex-1 w-full">
-          {!(CURRENT.style && CURRENT.feature) ? (
+          {!CURRENT.feature ? (
             <BackgroundImage />
           ) : (
             <ConstituencyView
               feature={CURRENT.feature}
-              color={CURRENT.style.fillColor}
+              color={CURRENT.style!.fillColor}
               candidate={CURRENT.candidate}
             />
           )}

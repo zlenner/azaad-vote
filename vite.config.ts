40c9894b741296258bbc2673edaabc69e0e9a00d
zlenner <119705166+zlenner@users.noreply.github.com>
2024-02-03 14:19:46 +0200

    Update download

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['html-to-image']
-  }
+  plugins: [react()]
 })

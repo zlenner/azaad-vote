8d5829a8cbfac3ae1a33de60915dc1f6391cfb7a
zlenner <119705166+zlenner@users.noreply.github.com>
2024-02-05 17:58:02 +0200

    Update issues.tsx

--- src/hooks/useData/issues.tsx
+++ src/hooks/useData/issues.tsx
@@ -1,3 +1,3 @@
 export const hardCodedProblematicSeats = ['NA-266', 'NA-11']
 
-export const knownIssues = ['NA-127', 'NA-96', 'NA-97', 'NA-84', 'NA-69']
+export const knownIssues = ['NA-127', 'NA-96', 'NA-97']
